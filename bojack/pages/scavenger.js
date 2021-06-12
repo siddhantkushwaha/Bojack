c3cd169fa0deaa6bb3d5c7fbde6ed452554212c7
siddhantkushwaha <k16.siddhant@gmail.com>
2021-06-12 19:48:57 +0530

    preferences

--- bojack/pages/scavenger.js
+++ bojack/pages/scavenger.js
@@ -1,17 +1,51 @@
 import style from './Scavenger.module.css'
+import {useState} from "react";
 
 export default function Scavenger() {
+
+    const [preferencesVisibilityState, setPreferencesVisibilityState] = useState(style.preferencesVisibility)
+    const togglePreferencesVisibility = () => {
+        if (preferencesVisibilityState === "")
+            setPreferencesVisibilityState(style.preferencesVisibility)
+        else
+            setPreferencesVisibilityState("")
+    }
+
     return (
         <div className="container">
             <div className="row align-items-center">
                 <div className="col-lg-9">
 
                     {/* Inputs related to search go here */}
-                    <form>
+                    <form method="GET">
                         <div>
                             <input type="text"
                                    className={`form-control border-0 bg-dark text-white shadow-none ${style.searchInput}`}
-                                   placeholder="Type to search."/>
+                                   placeholder="Type to search."
+                                   name="query"/>
+                        </div>
+                        <div className={`list-group list mt-2 ${style.preferences}`}>
+                            <button type="button" className={`list-group-item ${style.preferenceItem} border-bottom-0`}
+                                    onClick={togglePreferencesVisibility}>
+                                <i className={`bi bi-gear-fill`}/>
+                            </button>
+                            <div className={preferencesVisibilityState}>
+                                <label className={`list-group-item ${style.preferenceItem}`}>
+                                    <input className={`form-check-input me-2 ${style.preferenceInput}`} type="checkbox"
+                                           name="useRegex"/>
+                                    Use regular expressions
+                                </label>
+                                <label className={`list-group-item ${style.preferenceItem} border-bottom-0`}>
+                                    <input className={`form-check-input me-2 ${style.preferenceInput}`} type="radio"
+                                           name="termOperator" checked/>
+                                    Qualify if at least one query term is present
+                                </label>
+                                <label className={`list-group-item ${style.preferenceItem}`}>
+                                    <input className={`form-check-input me-2 ${style.preferenceInput}`} type="radio"
+                                           name="termOperator"/>
+                                    Qualify only if all query terms are present
+                                </label>
+                            </div>
                         </div>
                     </form>
 
@@ -32,7 +66,8 @@ export default function Scavenger() {
                             </div>
                             <p>Scavenger is built to organise all the work that I do on a daily basis on different
                                 platforms.</p>
-                            <p>The goal here is to make everything searchable and get some insights into how search engines work and answers to some important questions.</p>
+                            <p>The goal here is to make everything searchable and get some insights into how search
+                                engines work and answers to some important questions.</p>
                             <ul className={`px-5 py-3 ${style.infoList}`}>
                                 <li>How to collect and organize data from different sources?</li>
                                 <li>How to make querying easier?</li>
