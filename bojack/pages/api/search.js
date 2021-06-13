e4a03d1c335a53a21c18e935701064f26d8b0acb
siddhantkushwaha <k16.siddhant@gmail.com>
2021-06-13 20:13:38 +0530

    -

--- bojack/pages/api/search.js
+++ bojack/pages/api/search.js
@@ -3,7 +3,10 @@ import axios from "axios";
 export default async function handler(req, res) {
     try {
         console.log(req.query)
-        const data = await axios.get('http://localhost:8080/search', {params: req.query})
+        const data = await axios.get(
+            'http://sid.centralindia.cloudapp.azure.com:8080/search',
+            {params: req.query}
+        )
         res.status(200).json(data.data)
     } catch (error) {
         console.error(error)
